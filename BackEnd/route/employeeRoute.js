eeace83e727ccb197a10052499efc051c383b2fe
anushaanand19 <anushaanand1991@gmail.com>
2021-05-14 21:24:21 +0530

    added: auth middleware; implemented middleware for GET employees

--- BackEnd/route/employeeRoute.js
+++ BackEnd/route/employeeRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../controller/employeeController");
+const isAuth = require("../auth/auth").isAuth;
 
 router.post("/", employeeController.creatingEmployee);
 router.put("/:id", employeeController.updatingEmployee);
 router.get("/:id", employeeController.fetchingEmployee);
-router.get("/", employeeController.fetchingAllEmployees);
+router.get("/", isAuth, employeeController.fetchingAllEmployees);
 router.delete("/:id", employeeController.deletingEmployee);
 
 module.exports = router;
