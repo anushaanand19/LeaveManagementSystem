5dcda01c01aa98d21a719cd18556a0b8d11907e9
anushaanand19 <anushaanand1991@gmail.com>
2021-05-27 12:58:47 +0530

    changes to routing in employee component

--- BackEnd/route/employeeRoute.js
+++ BackEnd/route/employeeRoute.js
@@ -5,7 +5,7 @@ const isAuth = require("../auth/auth").isAuth;
 
 router.post("/", employeeController.creatingEmployee);
 router.put("/:id", employeeController.updatingEmployee);
-router.get("/:id", employeeController.fetchingEmployee);
+router.get("/:id", isAuth, employeeController.fetchingEmployee);
 router.get("/", isAuth, employeeController.fetchingAllEmployees);
 router.delete("/:id", employeeController.deletingEmployee);
 
